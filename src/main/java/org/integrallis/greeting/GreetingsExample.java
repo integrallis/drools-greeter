138c6522377fad0a820a2e8613d7fb3b8a1bd3f7
Brian Sam-Bodden <bsbodden@integrallis.com>
2015-01-09 11:13:17 -0700

    Step 4: Your first Rule and asserting Facts

--- src/main/java/org/integrallis/greeting/GreetingsExample.java
+++ src/main/java/org/integrallis/greeting/GreetingsExample.java
@@ -4,6 +4,10 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import static org.integrallis.greeting.Person.Education.*;
+import static org.integrallis.greeting.Person.Gender.*;
+import static org.integrallis.greeting.Person.MaritalStatus.*;
+
 /**
  * This is a sample class to launch a rule.
  */
@@ -24,7 +28,16 @@ public class GreetingsExample {
             kSession = kContainer.newKieSession("ksession-rules");
 
             // 4. - Assert some Facts (Person/TimeOfDay objects)
-            
+ 			Person stephenFalken = new Person("Stephen Falken", MALE, 48, SINGLE, DROPOUT);
+ 			Person richie = new Person("Richie Rich", MALE, 9, SINGLE, NONE);
+ 			Person marieCurie = new Person("Marie Curie", FEMALE, 45, MARRIED, PHD);
+ 			
+ 			TimeOfDay nineAm = new TimeOfDay(9, 0);
+
+ 			kSession.insert(marieCurie);
+ 			kSession.insert(stephenFalken);
+ 			kSession.insert(richie);
+ 			kSession.insert(nineAm);
             
             // 5. - Fire the Rules
             kSession.fireAllRules();
